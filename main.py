83d21303b1a5b1fe0294dc03a83aac2ac782f584
ajpert <84763013+ajpert@users.noreply.github.com>
2022-09-14 17:19:29 -0700

    better interface

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
-
 import turtle
 
 t = turtle.Turtle()
 t.speed(0)
+t.turtlesize(.5)
 
 bonk = ['l','l']
 
 num = 1;
-show_draw = True
+show_draw = False #change to false to speed up process, but does not draw
 if show_draw == False:
   turtle.tracer(0, 0)
-iteration = 5
+iteration = 15
 turtle.screensize(canvwidth=30000, canvheight=30000,
                   bg="white")
 for z in range(iteration):
@@ -33,5 +33,6 @@ for n in bonk:
   else:
     t.right(90)
   t.forward(5)
-
+t.hideturtle()
 turtle.update()
+turtle.exitonclick()
